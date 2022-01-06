a4fb295da24474a8f3dfc2688b81c6608de7ee46
Celine Joie V. Buenaventura <buenaventuraceline277@gmail.com>
2022-01-06 10:19:44 +0800

    Some simplifation on my own solution

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                '$questions[questionNumber]',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
